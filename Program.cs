8b9d70776d774cb23091f9629c3ee30e1d2f5d29
Loganleifur <45595622+Loganleifur@users.noreply.github.com>
2019-04-15 12:10:29 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -110,7 +110,7 @@ namespace Sans_dating_sim
                     Console.ReadKey();
                     Console.WriteLine("Sans: 'I'm gonna say fo sho' ");
                     Console.ReadKey();
-                    COnsole.WriteLine("You start to feel the rhythm");
+                    Console.WriteLine("You start to feel the rhythm");
                     Console.ReadKey();
                     Console.WriteLine("Sans: 'I'm gonna say the N-word!' ");
                     Console.ReadKey();
